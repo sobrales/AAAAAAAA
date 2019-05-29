37ab6d826239a4a00070369bff6132591584fab5
MauriceLord <rafasa99@usp.br>
2019-05-29 13:33:44 -0300

    teste 2 funfa

--- teste.py
+++ teste.py
@@ -33,10 +33,8 @@ def f_teste2(t,x):
     A = np.array([[-2,-1,-1,-2],[1,-2,2,-1],[-1,-2,-2,-1],[2,-1,1,-2]])
     return np.matmul(A,x)
 
-
 def main():
     h = 1e-5
-<<<<<<< HEAD
     escolha = '1'
     if escolha == '1':
         t0, tf = 1.05, 3
@@ -48,13 +46,4 @@ def main():
     print('Aproximação de Euler',euler)'''
     kutta = Runge_Kutta(t0,tf,x0,h)
     print('Aproximação de Runge Kutta',kutta)
-
-=======
-    e = Euler(t0,tf,x0,h)
-    r = Runge_Kutta(t0,tf,x0,h)
-    print('Aproximação de Euler',e)
-    print(np.e-e)
-    print('Aproximação de Runge Kutta',r)
-    print(np.e-r)
->>>>>>> 6549e04ea7262947e1942749f7cc773ba91ff530
 main()
--- testebral.py
+++ testebral.py
@@ -1,44 +1,87 @@
-def TesteA():
+import numpy as np
+import math
+
+def MetEuler(x0,t0,tf,h,F):
+    i=0
+    x=x0
+    t=t0
+    n=(tf-t0)/h
+    while i<=n:
+        x=x+h*F(t,x)
+        t=t0+i*h
+        i+=1
+    return x
+
+def Runge_Kutta(x0,t0,tf,h,F):
+    i=0
+    x=x0
+    t=t0
+    n=(tf-t0)/h
+    #integral=F(t0,x0)*h
+    integral=0
+    if type(x)!=np.ndarray:
+        while i<=n:
+            k1=h*F(t,x)
+            k2=h*F(t+0.5*h,x+0.5*k1)
+            k3=h*F(t+0.5*h,x+0.5*k2)
+            k4=h*F(t+h,x+k3)
+            x=x+(k1+2*k2+2*k3+k4)/6
+            t=t0+i*h
+            integral+=F(t,x)*h
+            i+=1
+    else:
+        while i<=n:
+            k1=np.dot(F(t,x),h)
+            k2=np.dot(F(t+0.5*h,x+np.dot(k1,0.5)),h)
+            k3=np.dot(F(t+0.5*h,x+np.dot(k2,0.5)),h)
+            k4=np.dot(F(t+h,x+k3),h)
+            x=x+np.divide(k1+np.dot(k2,2)+np.dot(k3,2)+k4,6)
+            t=t0+i*h
+            integral+=np.dot(F(t,x),h)
+            i+=1
+    return x,integral
+
+def TesteA():   
     def F(t,x):
         return 1+(x-t)**2
     x0=-18.95
     t0=1.05
     tf=3
     h=1e-5
-    print('a')
-    x=Runge_Kutta(x0,t0,tf,h)
-    print(x)
+    x,integral=Runge_Kutta(x0,t0,tf,h,F)
+    print(integral)
+    print(x-x0)
 
+def TesteB():
+    A=np.array([[-2,-1,-1,-2],[1,-2,2,-1],[-1,-2,-2,-1],[2,-1,1,-2]])
+    def F(t,x):
+        return np.dot(A,x)
+    x0=np.ones((4,1))
+    x0[3]=-1
+    t0=0
+    tf=2
+    h=1e-5
+    x,integral=Runge_Kutta(x0,t0,tf,h,F)
+    print(integral)
+    print(x-x0)
 
-def MetEuler(x0,t0,tf,h):
-
-    i=0
-    x=x0
-    t=t0
-    n=(tf-t0)/h
-    while i<n:
-        t=t+i*h
-        x=x+h*F(t,x)
-        i+=1
-    return x
-
-def Runge_Kutta(x0,t0,tf,h):
-    from TesteA import F
-    i=0
-    x=x0
-    t=t0
-    n=(tf-t0)/h
-    print('c')
-    while i<n:
-        print('b')
-        k1=h*F(t,x)
-        k2=h*F(t+0.5*h,x+0.5*k1)
-        k3=h*F(t+0.5*h,x+0.5*k2)
-        k4=h*F(t+h,x+k3)
-        x=x+(k1+2*k2+2*k3+k4)/6
-        i+=1
-    return x
+def TesteC():
+    n=int(input("Escolha o tamanho da matriz A que deseja"))
+    A=np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            if abs(i-j)==1:
+                A[i][j]=1
+            elif i==j:
+                A[i][j]=-2
+    x0=np.zeros(n)
+    for i in range(n):
+        a=(i+1)/(n+1)
+        x0[i]=math.sin(math.pi*a)+math.sin(n*math.pi*a)
+    def F(t,x):
+        return np.dot(A,X)
+        
 
 def main():
-    TesteA()
+    TesteC()
 main()
